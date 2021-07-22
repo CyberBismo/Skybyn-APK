59373a19712374a0461a706f80b428ac39527f4e
Lars Einar Bismo <l.e.b@outlook.com>
2021-07-22 18:54:34 +0200

    Optimizations

--- app/src/main/java/com/example/wesocial/FrontPage.java
+++ app/src/main/java/com/example/wesocial/FrontPage.java
@@ -1,32 +1,22 @@
 package com.example.wesocial;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.hardware.biometrics.BiometricPrompt;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.toolbox.JsonObjectRequest;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class FrontPage extends AppCompatActivity {
     SharedPreferences sharedpreferences;
-    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front_page);
         sharedpreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
-        logout = findViewById(R.id.logout);
+        Button logout = findViewById(R.id.logout);
         logout.setOnClickListener(v -> logOut());
 
 
@@ -36,7 +26,7 @@ public class FrontPage extends AppCompatActivity {
         //DELETE ALL keys
         SharedPreferences.Editor sharedPrefEditor = sharedpreferences.edit();
         sharedPrefEditor.clear();
-        sharedPrefEditor.commit();
+        sharedPrefEditor.apply();
 
         Intent intent = new Intent(this, LoginRegisterForgot.class);
         startActivity(intent);
