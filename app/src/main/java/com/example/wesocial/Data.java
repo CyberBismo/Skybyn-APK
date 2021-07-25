65dd8ed91222b58eed9ed8c677cb3b9d2f642cab
cyberwizard-dev <53139650+cyberwizard-dev@users.noreply.github.com>
2021-07-25 10:01:02 +0100

    Added Navigation Drawer to the Frontpage
    Dummy Menu file added to the side Navigation Drawer....

--- app/src/main/java/com/example/wesocial/Data.java
+++ app/src/main/java/com/example/wesocial/Data.java
@@ -3,15 +3,15 @@ package com.example.wesocial;
 public class Data {
     public String version_url = "https://wesocial.space/apk/version.php";
     public String apk_url = "https://wesocial.space/apk/WeSocial.apk";
-    public String forgotpassword_url = "https://wesocial.space/mob_api?forgot=";
+    public String forgotpassword_url = "https://wesocial.space/mob_api";
     //public String verifyAccount_url = "https://wesocial.space/mob_api?forgot=";
 
     public String register_url (){
-        return "https://wesocial.space/mob_api?register&email";
+        return "https://wesocial.space/mob_api";
     }
 
     public String verifyAccountUrl(){
-        return "https://wesocial.space/mob_api?verify";
+        return "https://wesocial.space/mob_api";
     }
 
     public String getApkName(){
--- app/src/main/java/com/example/wesocial/Frontpage.java
+++ app/src/main/java/com/example/wesocial/Frontpage.java
@@ -7,23 +7,29 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import static com.example.wesocial.R.*;
 
 
 public class Frontpage extends AppCompatActivity {
     SharedPreferences sharedpreferences;
+    DrawerLayout drawerLayout;
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_front_page);
-        sharedpreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
+        setContentView(layout.activity_front_page);
+        sharedpreferences = getSharedPreferences(getString(string.app_name), Context.MODE_PRIVATE);
 
         configureToolbarAndDrawer();
     }
@@ -31,21 +37,51 @@ public class Frontpage extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.top_menu,menu);
+        inflater.inflate(R.menu.top_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+
+            case id.Logout:
+                logOut();
+                break;
+
+
+            default:
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
     }
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     public void configureToolbarAndDrawer() {
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setTitle(R.string.app_name);
-        DrawerLayout drawerLayout = findViewById(R.id.drawerlayout);
-        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        toolbar.setTitle(string.app_name);
+        drawerLayout = findViewById(id.drawerlayout);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, string.nav_open, string.nav_close);
         drawerLayout.addDrawerListener(drawerToggle);
         drawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.hamburger);
+        getSupportActionBar().setHomeAsUpIndicator(drawable.hamburger);
 
     }
 
