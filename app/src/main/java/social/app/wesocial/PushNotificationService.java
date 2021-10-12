fee8f61064de985fc3c54315ff3787f9470ec51f
cyberwizard-dev <53139650+cyberwizard-dev@users.noreply.github.com>
2021-10-12 13:43:47 +0100

    Update PushNotificationService.java

--- app/src/main/java/social/app/wesocial/PushNotificationService.java
+++ app/src/main/java/social/app/wesocial/PushNotificationService.java
@@ -1,29 +1,18 @@
 package social.app.wesocial;
 
-import static social.app.wesocial.Frontpage.userID;
-
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.os.Build;
-import android.util.Log;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.android.volley.VolleyError;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.installations.FirebaseInstallations;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 import java.util.HashMap;
-import java.util.Locale;
 
 import timber.log.Timber;
 
@@ -32,11 +21,9 @@ public class PushNotificationService extends FirebaseMessagingService {
     public static String userNotificationtoken;
     public static String weSocial_Topic = "wesocial";
     public Data data = new Data();
-    enum pushNotificationTypes{chat,general};
     int notificationId;
+
     Boolean gottenToken = false;
-
-
     public PushNotificationService() {
         FirebaseMessaging.getInstance().getToken()
                 .addOnCompleteListener(task -> {
@@ -44,10 +31,9 @@ public class PushNotificationService extends FirebaseMessagingService {
                         //Log.w(TAG, "Fetching FCM registration token failed", task.getException());
                         return;
                     }
-
                     // Get new FCM registration token
                     String token = task.getResult();
-                    Timber.i("TOKEN"+token);
+                    //Timber.i("TOKEN" + token);
                     NetworkController networkController = new NetworkController(getApplicationContext(), new NetworkController.IResult() {
                         @Override
                         public void notifySuccess(String response) {
@@ -58,14 +44,10 @@ public class PushNotificationService extends FirebaseMessagingService {
                         public void notifyError(VolleyError error) {
 
                         }
-
                     });
                     HashMap<String, String> postData = new HashMap<>();
-                    postData.put("token",token);
+                    postData.put("token", token);
                     networkController.PostMethod(data.updateToken_Api, postData);
-
-
-
 
 
                 });
@@ -83,33 +65,37 @@ public class PushNotificationService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.notifications_coloured)
-                .setContentTitle(remoteMessage.getData().get("title"))
-                .setContentText(remoteMessage.getData().get("body"))
-                .setBadgeIconType(NotificationCompat.BADGE_ICON_SMALL)
-                .setAutoCancel(true)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
 
         if (remoteMessage.getData().containsKey("type")) {
             switch (remoteMessage.getData().get("type")) {
                 case "chat":
                     String fromID = remoteMessage.getData().get("from");
-                    notificationId = 1;
+                    builder.setSmallIcon(R.drawable.chat);
+
+                default:
+                    break;
+
             }
-        }else{
-            notificationId = 999;
-        }
 
-      //  if (!Frontpage.isVisible) {
+
+            builder.setContentTitle(remoteMessage.getData().get("title"))
+                    .setContentText(remoteMessage.getData().get("body"))
+                    .setBadgeIconType(NotificationCompat.BADGE_ICON_SMALL)
+                    .setAutoCancel(true)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+
+            //  if (!Frontpage.isVisible) {
             // notificationId is a unique int for each notification that you must define
+            notificationId = 9999;
             notificationManager.notify(notificationId, builder.build());
-        
-     //   }
+
+            //   }
 
 
+        }
     }
 
     private void createNotificationChannel() {
@@ -128,10 +114,12 @@ public class PushNotificationService extends FirebaseMessagingService {
         }
     }
 
-
     @Override
     public void onNewToken(String s) {
         super.onNewToken(s);
 
     }
+
+
+    enum pushNotificationTypes {chat, general}
 }
